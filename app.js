423899ed9670df4a9c74a738cbd66038213ce3a1
Md Adnan <sirgeant10@gmail.com>
2024-05-29 14:51:12 +0600

    Create basic functions and call them.

--- app.js
+++ app.js
@@ -1,1 +1,29 @@
-console.log("Hello world");
+const numberA = 10;
+const numberB = 5;
+const operator = "div";
+
+const add = (a, b) => a + b;
+const sub = (a, b) => a - b;
+const mul = (a, b) => a * b;
+const div = (a, b) => a / b;
+
+function operate(numA, numB, op) {
+  switch (op) {
+    case "add":
+      console.log(add(numA, numB));
+      break;
+    case "sub":
+      console.log(sub(numA, numB));
+      break;
+    case "mul":
+      console.log(mul(numA, numB));
+      break;
+    case "div":
+      console.log(div(numA, numB));
+      break;
+    default:
+      console.log("Some Error Occured");
+  }
+}
+
+operate(numberA, numberB, operator)
